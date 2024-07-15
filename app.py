c903473e4e2b78de9dd530ab9a80aae983e670db
R A Z i <119275831+MeeRazi@users.noreply.github.com>
2024-07-15 22:50:11 +0530

    update

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from quart import Quart, request, jsonify, render_template
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 import os
+import random
 
 app = Quart(__name__)
 
 # MongoDB connection
-client = AsyncIOMotorClient(os.environ.get("MONGODB_URL", ""))
+client = AsyncIOMotorClient(os.environ.get("MONGODB_URL"))
 db = client.floating_notes
 notes_collection = db.notes
+
+# List of pastel colors for notes
+COLORS = [
+    "#FFB3BA", "#BAFFC9", "#BAE1FF", "#FFFFBA", "#FFDFBA",
+    "#E0BBE4", "#957DAD", "#D291BC", "#FEC8D8", "#FFDFD3"
+]
 
 @app.route('/')
 async def index():
@@ -16,7 +23,9 @@ async def index():
 
 @app.route('/api/notes', methods=['GET'])
 async def get_notes():
-    notes = await notes_collection.find().to_list(length=None)
+    search_query = request.args.get('search', '')
+    query = {"content": {"$regex": search_query, "$options": "i"}} if search_query else {}
+    notes = await notes_collection.find(query).to_list(length=None)
     return jsonify([{**note, '_id': str(note['_id'])} for note in notes])
 
 @app.route('/api/notes', methods=['POST'])
@@ -25,11 +34,23 @@ async def add_note():
     note = {
         'content': data['content'],
         'likes': 0,
-        'dislikes': 0
+        'dislikes': 0,
+        'color': random.choice(COLORS)
     }
     result = await notes_collection.insert_one(note)
     note['_id'] = str(result.inserted_id)
     return jsonify(note), 201
+
+@app.route('/api/notes/<note_id>', methods=['PUT'])
+async def update_note(note_id):
+    data = await request.json
+    await notes_collection.update_one({'_id': ObjectId(note_id)}, {'$set': {'content': data['content']}})
+    return '', 204
+
+@app.route('/api/notes/<note_id>', methods=['DELETE'])
+async def delete_note(note_id):
+    await notes_collection.delete_one({'_id': ObjectId(note_id)})
+    return '', 204
 
 @app.route('/api/notes/<note_id>/like', methods=['POST'])
 async def like_note(note_id):
